8c2585684df3593e39d49721dd0863d3b71a88ae
EhsanTelgerdy <ehsan.telgerdy@geeks.ltd.uk>
2022-07-21 11:37:10 +0430

    modifying referencedata file

--- Domain/[DEV-SCRIPTS]/ReferenceData.cs
+++ Domain/[DEV-SCRIPTS]/ReferenceData.cs
@@ -21,47 +21,14 @@ namespace Domain
             await Create(new Settings { Name = "Current", PasswordResetTicketExpiryMinutes = 2 });
 
             await CreateAdmins();
-            await CreateContacts();
         }
 
         async Task CreateAdmins()
         {
-            await AddAdmin("Oliver", "Jones", "oiver@fakemail.com");
-            await AddAdmin("Daniel", "Williams", "daniel@office.edu");
-            await AddAdmin("Thomas", "Davies", "thomas@uat.co");
-            await AddAdmin("Harry", "Evans", "harry@williams.com");
-            await AddAdmin("Jack", "Roberts", "jack@for-test.net");
-            await AddAdmin("Samuel", "Morgan", "samuel@college.edu");
-            await AddAdmin("James", "Edwards", "james@humour.org");
-            await AddAdmin("Alexander", "Smith", "alexander@fakemail.com");
-            await AddAdmin("Charlie", "Phillips", "charlie@office.edu");
-            await AddAdmin("Emma", "Richards", "emma@uat.co");
+#pragma warning disable GCop646 // Email addresses should not be hard-coded. Move this to Settings table or Config file.
+            await AddAdmin("test", "testington", "admin@uat.co");
         }
 
-        async Task CreateContacts()
-        {
-            await AddContact("Oliver", "Jones", "020 8549 1245");
-            await AddContact("Daniel", "Williams", "084 5264 8548");
-            await AddContact("Thomas", "Davies", "020 7569 3254");
-            await AddContact("Harry", "Evans", "0127 7786 5314");
-            await AddContact("Jack", "Roberts", "079 8556 7059");
-            await AddContact("Samuel", "Morgan", "0800 6325 978");
-            await AddContact("James", "Edwards", "0109 6455 2135");
-            await AddContact("Alexander", "Smith", "078 9563 2157");
-            await AddContact("Charlie", "Phillips", "0203 6654 162");
-            await AddContact("Emma", "Richards", "020 8549 1245");
-            await AddContact("Andrew", "Richards", "020 9587 8765");
-        }
-
-        private Task<Contact> AddContact(string firstName, string lastName, string phone)
-        {
-            return Create(new Contact
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                PhoneNumber = phone
-            });
-        }
 
         private Task<Administrator> AddAdmin(string firstName, string lastName, string email)
         {
